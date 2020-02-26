2cfa1201feb74dfa628a7efa57ca3bb1ba7e4af4
zhangtianxu <a823525514@gmail.com>
2020-02-26 09:04:42 +0800

    'Test1'

---
+++ src/main/java/com/ztx/mavendemo2/App.java
@@ -0,0 +1,13 @@
+package com.ztx.mavendemo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
